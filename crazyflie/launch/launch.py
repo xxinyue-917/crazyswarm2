ec415695e3662e19ceb2f07c0242f0a792e8daf5
Wolfgang Hoenig <hoenig@tu-berlin.de>
2024-03-06 21:04:43 +0100

    launch.py: disable joystick teleop by default

--- crazyflie/launch/launch.py
+++ crazyflie/launch/launch.py
@@ -87,11 +87,12 @@ def generate_launch_description():
             name='teleop',
             remappings=[
                 ('emergency', 'all/emergency'),
-                ('takeoff', 'cf6/takeoff'),
-                ('land', 'cf6/land'),
-                ('cmd_vel_legacy', 'cf6/cmd_vel_legacy'),
-                ('cmd_full_state', 'cf6/cmd_full_state'),
-                ('notify_setpoints_stop', 'cf6/notify_setpoints_stop'),
+                ('takeoff', 'all/takeoff'),
+                ('land', 'all/land'),
+                # uncomment to manually control (and update teleop.yaml)
+                # ('cmd_vel_legacy', 'cf6/cmd_vel_legacy'),
+                # ('cmd_full_state', 'cf6/cmd_full_state'),
+                # ('notify_setpoints_stop', 'cf6/notify_setpoints_stop'),
             ],
             parameters=[teleop_params]
         ),
