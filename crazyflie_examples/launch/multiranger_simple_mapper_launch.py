8d32d5d37d1bd027445d0854d3f0a3d4827c8ff9
knmcguire <kimberly@bitcraze.io>
2024-10-01 10:28:37 +0200

    fix ci issues

--- crazyflie_examples/launch/multiranger_simple_mapper_launch.py
+++ crazyflie_examples/launch/multiranger_simple_mapper_launch.py
@@ -2,9 +2,9 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -24,13 +24,6 @@ def generate_launch_description():
 
     return LaunchDescription([
         crazyflie,
-        Node(
-            package='crazyflie',
-            executable='crazyflie_server.py',
-            name='crazyflie_server',
-            output='screen',
-            parameters=[server_params]
-        ),
         Node(
             package='crazyflie',
             executable='vel_mux.py',
