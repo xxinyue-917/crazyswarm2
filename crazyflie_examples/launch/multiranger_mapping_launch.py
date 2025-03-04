b6f67499f7e0e70d2fd5ee0a92f8c935a17d2415
knmcguire <kimberleymcguire@gmail.com>
2025-03-04 13:44:51 +0100

    update format examples

--- crazyflie_examples/launch/multiranger_mapping_launch.py
+++ crazyflie_examples/launch/multiranger_mapping_launch.py
@@ -9,34 +9,49 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('crazyflie'), 'launch'),
-            '/launch.py']),
-        launch_arguments={
-            'backend': 'cflib',
-            'gui': 'false',
-            'teleop': 'false',
-            'mocap': 'false',
-            }.items()),
-        Node(
-            package='crazyflie',
-            executable='vel_mux.py',
-            name='vel_mux',
-            output='screen',
-            parameters=[{'hover_height': 0.3},
-                        {'incoming_twist_topic': '/cmd_vel'},
-                        {'robot_prefix': '/cf231'}]
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(get_package_share_directory('slam_toolbox'), 
-                                                         'launch/online_async_launch.py')),
-            launch_arguments={
-                'slam_params_file': os.path.join(get_package_share_directory('crazyflie_examples'),
-                'config/slam_params.yaml'),
-                'use_sim_time': 'False',
-            }.items()
-        ),
-    ])
+    return LaunchDescription(
+        [
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        os.path.join(
+                            get_package_share_directory('crazyflie'), 'launch'
+                        ),
+                        '/launch.py',
+                    ]
+                ),
+                launch_arguments={
+                    'backend': 'cflib',
+                    'gui': 'false',
+                    'teleop': 'false',
+                    'mocap': 'false',
+                }.items(),
+            ),
+            Node(
+                package='crazyflie',
+                executable='vel_mux.py',
+                name='vel_mux',
+                output='screen',
+                parameters=[
+                    {'hover_height': 0.3},
+                    {'incoming_twist_topic': '/cmd_vel'},
+                    {'robot_prefix': '/cf231'},
+                ],
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(
+                        get_package_share_directory('slam_toolbox'),
+                        'launch/online_async_launch.py',
+                    )
+                ),
+                launch_arguments={
+                    'slam_params_file': os.path.join(
+                        get_package_share_directory('crazyflie_examples'),
+                        'config/slam_params.yaml',
+                    ),
+                    'use_sim_time': 'False',
+                }.items(),
+            ),
+        ]
+    )
--- crazyflie_examples/launch/multiranger_simple_mapper_launch.py
+++ crazyflie_examples/launch/multiranger_simple_mapper_launch.py
@@ -10,35 +10,42 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     crazyflie = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('crazyflie'), 'launch'),
-            '/launch.py']),
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(get_package_share_directory('crazyflie'), 'launch'),
+                '/launch.py',
+            ]
+        ),
         launch_arguments={
             'backend': 'cflib',
             'gui': 'false',
             'teleop': 'false',
             'mocap': 'false',
-            }.items())
+        }.items(),
+    )
 
     crazyflie_name = '/cf231'
 
-    return LaunchDescription([
-        crazyflie,
-        Node(
-            package='crazyflie',
-            executable='vel_mux.py',
-            name='vel_mux',
-            output='screen',
-            parameters=[{'hover_height': 0.3},
-                        {'incoming_twist_topic': '/cmd_vel'},
-                        {'robot_prefix': crazyflie_name}]
-        ),
-        Node(
-            package='crazyflie',
-            executable='simple_mapper_multiranger.py',
-            name='simple_mapper_multiranger',
-            output='screen',
-            parameters=[
-                {'robot_prefix': crazyflie_name}]
-        ),
-    ])
+    return LaunchDescription(
+        [
+            crazyflie,
+            Node(
+                package='crazyflie',
+                executable='vel_mux.py',
+                name='vel_mux',
+                output='screen',
+                parameters=[
+                    {'hover_height': 0.3},
+                    {'incoming_twist_topic': '/cmd_vel'},
+                    {'robot_prefix': crazyflie_name},
+                ],
+            ),
+            Node(
+                package='crazyflie',
+                executable='simple_mapper_multiranger.py',
+                name='simple_mapper_multiranger',
+                output='screen',
+                parameters=[{'robot_prefix': crazyflie_name}],
+            ),
+        ]
+    )
