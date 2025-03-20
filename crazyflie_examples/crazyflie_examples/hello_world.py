20a01b076bc438d8c118e5579f82cf9ac5ef194f
xxinyue <xxinyue@umich.edu>
2025-03-19 22:00:39 -0400

    Initialize the simulation of swarmalators

--- crazyflie_examples/crazyflie_examples/hello_world.py
+++ crazyflie_examples/crazyflie_examples/hello_world.py
@@ -11,16 +11,16 @@ def main():
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     cf1 = swarm.allcfs.crazyflies[0]
-    cf2 = swarm.allcfs.crazyflies[1]
+    # cf2 = swarm.allcfs.crazyflies[1]
 
     cf1.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
-    timeHelper.sleep(TAKEOFF_DURATION)
-    cf2.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
+    # timeHelper.sleep(TAKEOFF_DURATION)
+    # cf2.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
     timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
     cf1.land(targetHeight=0.04, duration=2.5)
     timeHelper.sleep(TAKEOFF_DURATION) 
-    cf2.land(targetHeight=0.04, duration=2.5)
-    timeHelper.sleep(TAKEOFF_DURATION)
+    # cf2.land(targetHeight=0.04, duration=2.5)
+    # timeHelper.sleep(TAKEOFF_DURATION)
 
 
 if __name__ == '__main__':
